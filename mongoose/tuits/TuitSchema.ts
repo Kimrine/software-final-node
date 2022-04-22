894114b9ca46e912fcd4cf00c173d7af25b72139
Zhaoyan <irisfeng0321@outlook.com>
2022-04-22 15:10:40 -0400

    update documentation

--- mongoose/tuits/TuitSchema.ts
+++ mongoose/tuits/TuitSchema.ts
@@ -4,13 +4,13 @@
 import mongoose, {Schema} from "mongoose";
 import Tuit from "../../models/tuits/Tuit";
 
-
-
 /**
  * @typedef Tuit Represents tuits
  * @property {string} tuit the content of tuit
  * @property {ObjectId} postedBy the User Id who post the tuit
  * @property {date} postedOn the post time of tuit
+ * @property {String} image the link of image
+ * @property {String} youtube the link of video
  */
 const TuitSchema = new mongoose.Schema<Tuit>({
 
